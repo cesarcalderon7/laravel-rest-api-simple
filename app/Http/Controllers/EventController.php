af254065335ffc99deed20106a664302fcbf2055
CesarCast <cesarce735@gmail.com>
2022-06-08 12:47:58 -0400

    Añadir respuesta 404 para eventos de retirada (cuentas no existentes)

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -14,6 +14,11 @@ class EventController extends Controller
                 $request->input('destination'),
                 $request->input('amount')
             );
+        }elseif($request->input('type') === 'withdraw'){
+            return $this -> withdraw(
+                $request->input('origin'),
+                $request->input('amount')
+            );
         }
     }
     
@@ -31,5 +36,12 @@ class EventController extends Controller
                 'balance' => $account->balance
           ]
         ], 201);
+    }
+
+    private function withdraw($origin, $amount )
+    {
+        $account = Account::findOrFail($origin);
+
+  
     }
 }
